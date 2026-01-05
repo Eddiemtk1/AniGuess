5dcc267661c29cf0b7bf2bd3ef888662044f3c64
Eddie <eddiemtk@gmail.com>
2026-01-05 18:25:20 +0000

    fixed profile screen

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -20,8 +20,7 @@ class _ProfileScreen extends State<ProfileScreen> {
   final User? user = FirebaseAuth.instance.currentUser;
   bool isLoading = true;
   Map<String, dynamic>? userData;
-      Uint8List? profileImageBytes;
-
+  Uint8List? profileImageBytes;
 
   @override
   void initState() {
@@ -39,7 +38,7 @@ class _ProfileScreen extends State<ProfileScreen> {
       if (documentSnapshot.exists) {
         setState(() {
           userData = documentSnapshot.data() as Map<String, dynamic>?;
-          if(userData? ["photoBase64"] !=null){
+          if (userData?["photoBase64"] != null) {
             profileImageBytes = base64Decode(userData!["photoBase64"]);
           }
           isLoading = false;
@@ -49,28 +48,35 @@ class _ProfileScreen extends State<ProfileScreen> {
       print(e.toString());
       showSnackBAR(context, "Error getting user data:$e");
       setState(() {
-        isLoading =false;
+        isLoading = false;
       });
     }
   }
-  Future<void>updateProfileImage(Uint8List imageBytes) async{
-    if(user == null)return;
-    try{
+
+  Future<void> updateProfileImage(Uint8List imageBytes) async {
+    if (user == null) return;
+    try {
       String base64Image = base64Encode(imageBytes);
       //ensure the photobase64 is only updated when non-null
-      await FirebaseFirestore.instance.collection("userData").doc(user!.uid).set({"photoBase64":base64Image}, SetOptions(merge: true),);
+      await FirebaseFirestore.instance
+          .collection("userData")
+          .doc(user!.uid)
+          .set({"photoBase64": base64Image}, SetOptions(merge: true));
       setState(() {
         profileImageBytes = imageBytes;
       });
       showSnackBAR(context, "Profile image updated successfully");
-    }catch(e){
+    } catch (e) {
       showSnackBAR(context, "Failed to update profile image-_-: $e");
     }
   }
+
   //Choose image from gallery
-  Future<void> pickImageFromGallery() async{
-    final returnImage = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if(returnImage == null)return;
+  Future<void> pickImageFromGallery() async {
+    final returnImage = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+    if (returnImage == null) return;
     //Convert image to bytes
     final imageBytes = await returnImage.readAsBytes();
     if (!mounted) return;
@@ -96,57 +102,72 @@ class _ProfileScreen extends State<ProfileScreen> {
           ? Center(child: CircularProgressIndicator())
           : userData == null
           ? Center(child: Text("No user data found"))
-          : Padding(
-              padding: EdgeInsets.all(15),
-              child: Column(
-                children: [
-                  GestureDetector(
-                    child: CircleAvatar(
-                      backgroundColor: Colors.blue[100],
-                      radius: 60,
-                      backgroundImage:
-                          const NetworkImage(
-                                "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
-                              )
-                              as ImageProvider,
-                      child: Align(
-                        alignment: AlignmentGeometry.bottomRight,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.black,
-                          radius: 16,
-                          child: Icon(
-                            Icons.camera_alt,
-                            size: 20,
-                            color: Colors.white,
+          : Center(
+              child: Padding(
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: pickImageFromGallery,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.blue[100],
+                        radius: 60,
+                        backgroundImage: profileImageBytes != null
+                            ? MemoryImage(profileImageBytes!)
+                            : const NetworkImage(
+                                    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
+                                  )
+                                  as ImageProvider,
+                        child: Align(
+                          alignment: AlignmentGeometry.bottomRight,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.black,
+                            radius: 16,
+                            child: Icon(
+                              Icons.camera_alt,
+                              size: 20,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 15),
-                  Text(
-                    userData?["name"],
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    "Score: ${userData?["score"] * 102}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  Divider(),
-                  const SizedBox(height: 15),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: MyButton(onTap: signOut, buttontext: "Sign Out"),
+                    const SizedBox(height: 15),
+                    Text(
+                      userData?["name"],
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      "Score: ${userData?["score"] * 7 ?? 0}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Divider(),
+                    const SizedBox(height: 15),
+                    Center(
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: MyButton(
+                              onTap: signOut,
+                              buttontext: "Sign Out",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
     );
